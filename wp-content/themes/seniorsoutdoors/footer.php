fa26599e2db5a96b9287e31d67b550b4256116a3
Liz Burton <liz.burton@comcast.net>
2015-12-12 09:37:40 -0500

    hacky but minimally functional version of slider

--- wp-content/themes/seniorsoutdoors/footer.php
+++ wp-content/themes/seniorsoutdoors/footer.php
@@ -17,32 +17,85 @@
 
 <?php wp_footer(); ?>
 <script type="text/javascript" charset="utf-8">
-	$("#header-slider").find("div:first").addClass("current").next().addClass("next"); 
+//	$("#header-slider").find("div:first").addClass("current").next().addClass("next"); 
+
+//Initial placement of images
 	var numberOfImages = $("#header-slider div").length,
-		currentImage,
-		imageId,
-		imageWidth,
+		counter = 0,
 		totalWidth = 0,
+		currentImage,
+		currentImageId,
+		currentImageWidth,
+		setWidth,
+		firstImageId,
 		firstImage,
-		slideAmount;
-	for (counter = 1; counter<=numberOfImages; counter++) {
-		imageId = "galleryImage" + counter;
-		currentImage = document.getElementById(imageId);
-		imageWidth = currentImage.clientWidth;
-		console.log(imageWidth);
-		totalWidth = totalWidth + imageWidth; 
-		if (totalWidth < 1080) {
-			$("#" + imageId).parent().addClass("visible");
+		firstImageWidth,
+		nextImageID,
+		nextImage,
+		nextImageWidth;
+	while (totalWidth < 1000) {
+		counter++;
+		currentImageId = "galleryImage" + counter;
+		currentImage = document.getElementById(currentImageId);
+		currentImageWidth = currentImage.clientWidth;
+		totalWidth = totalWidth + currentImageWidth; 
+		if (totalWidth < 1000) {
+			$("#" + currentImageId).parent().removeClass("invisible").addClass("visible");
+		} else {
+			setWidth = totalWidth - currentImageWidth;
+			counter = counter - 1;
+			break;
 		}
 	}
-//	window.setInterval(runSlider, 3000);
+//End inital placement
 
-//	function runSlider() {
-		imageId = $(".visible:first").find("img").attr("id");
-		firstImage = document.getElementById(imageId);
-		slideAmount = "-" + firstImage.clientWidth + "px";
-		console.log(slideAmount);
-		$(firstImage).addClass("test").animate({left: slideAmount}, 500);
+window.setInterval(runSlider, 5000);
+
+/*$('.slides').click(function() {
+
+    $(this).parent().animate({
+        left: '-50%'
+    }, 500, function() {
+        $(this).css('left', '150%');
+        $(this).appendTo('#header-slider');
+    });
+
+    $(this).parent().next().animate({
+        left: '50%'
+    }, 500);
+});
+*/
+function runSlider() {
+	if (counter >= 3) {
+		firstImageId = "galleryImage" + (counter - 2);
+	} else {
+		firstImageId = "galleryImage" + (numberOfImages + counter - 2);
+	}
+	firstImage = document.getElementById(firstImageId);
+	firstImageWidth = firstImage.clientWidth;
+	setWidth = setWidth - firstImageWidth;
+	$(firstImage).parent().removeClass("visible").addClass("invisible").css("display","none");
+	if (counter < numberOfImages) {
+		nextImageID = "galleryImage" + (counter + 1);
+	} else {
+		nextImageID = "galleryImage1";
+		counter = 0; 
+	}
+	nextImage = document.getElementById(nextImageID); 
+//	$("#" + nextImageID).parent().css("display","block");
+	nextImageWidth = nextImage.clientWidth;
+//	$("#" + nextImageID).parent().css("display","none");
+	console.log(nextImageWidth);
+	setWidth = setWidth + nextImageWidth;
+//	if (setWidth < 1000) {
+		$("#" + nextImageID).parent().removeClass("invisible").addClass("visible").css("display","block");
+//	}
+
+	counter ++;
+//	}
+}
+//		slideAmount = "-" + firstImage.clientWidth + "px";
+//		$(firstImage).addClass("test").animate({left: slideAmount}, 500);
 /*		if (counter < numberOfImages) {
 			$(".current").css("display","block").next().addClass("next").css("display","block"); //change animation so everything right of #1 moves left imageWidth pixels; 
 //			$(".current").slideUp(5000, function() {											//use z-index to make 1st image appear below the others
